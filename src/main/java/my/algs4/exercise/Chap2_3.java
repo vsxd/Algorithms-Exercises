c2d95ca14e5720016de4fa48d966967b788fb1d1
XudongX <sun.xd@hotmail.com>
2020-04-20 23:07:33 +0800

    Chap2.3

--- src/main/java/my/algs4/exercise/Chap2_3.java
+++ src/main/java/my/algs4/exercise/Chap2_3.java
@@ -1,5 +1,38 @@
 package my.algs4.exercise;
 
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Chap2_3 {
 }
 
+class QucikSort extends SortBase {
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo)  // 相等条件是必要，否则可能无法结束递归
+            return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+}
