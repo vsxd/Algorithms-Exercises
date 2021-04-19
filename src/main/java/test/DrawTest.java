7044827889011070347047b776589bd2ff4e73f9
Xudong <sun.xd@hotmail.com>
2021-04-19 21:34:28 +0100

    refactor structure

---
+++ src/main/java/test/DrawTest.java
@@ -0,0 +1,22 @@
+package main.java.test;
+
+import main.java.algs4.StdDraw;
+import main.java.algs4.StdRandom;
+
+
+public class DrawTest {
+    public static void main(String[] args) {
+        int N = 50;
+        double[] a = new double[N];
+        for (int i = 0; i < N; i++)
+            a[i] = StdRandom.random();
+//        Arrays.sort(a);
+        for (int i = 0; i < N; i++) {
+            double x = 1.0 * i / N;
+            double y = a[i] / 2.0;
+            double rw = 0.5 / N;
+            double rh = a[i] / 2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+}
